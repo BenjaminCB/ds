8217d22632c8b603d026d3111cb189137daafa27
bcb <benjamincb@hotmail.dk>
2023-09-03 20:41:39 +0200

    add dev shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+    description = "Flake utils demo";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = nixpkgs.legacyPackages.${system};
+                python-pkgs = pkgs: with pkgs; [
+                    pyqt6
+                    pynput
+                    cryptography
+                ];
+            in {
+                packages = rec {
+                    default = pkgs.hello;
+                };
+                devShells.default = pkgs.mkShell {
+                    nativeBuildInputs = [
+                        (pkgs.python3.withPackages python-pkgs)
+                        pkgs.python311Packages.python-lsp-server
+                        pkgs.just
+                    ];
+
+                    shellHook = ''
+                        echo Welcome to distributed systems shell
+                    '';
+                };
+            }
+        );
+}
